1e633804dc689ff4c3e447ff23d1d6d759c07185
AwsManas <manassharma6301@gmail.com>
2019-07-19 02:00:23 +0530

    Finishing!

--- shooter.py
+++ shooter.py
@@ -19,7 +19,6 @@ clock = pygame.time.Clock()
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder,"shooter-graphics")
 song_folder = os.path.join(game_folder,"sounds")
-all_sprites= pygame.sprite.Group()
 # Loading all game graphics 
 background = pygame.image.load(os.path.join(img_folder,"back.png")).convert()
 background_rect=background.get_rect()
@@ -32,10 +31,15 @@ asteroid_images = []
 asteroid_list = ["ast1.png","ast2.png","ast3.png","ast4.png"]
 for img in asteroid_list:
     asteroid_images.append(pygame.image.load(os.path.join(img_folder,img)).convert())
+power_img={}
+power_img['gun'] = pygame.image.load(os.path.join(img_folder,'bolt_gold.png')).convert()
+power_img['health'] = pygame.image.load(os.path.join(img_folder,'pill_green.png')).convert()
+power_img['live'] = pygame.image.load(os.path.join(img_folder,'powerupRed_star.png')).convert()    
 #Loading sounds 
 shoot_sound = pygame.mixer.Sound(os.path.join(song_folder,"bf.wav"))
 explo_sound = pygame.mixer.Sound(os.path.join(song_folder,'explosion.wav'))
-background_sound = pygame.mixer.music.load(os.path.join(song_folder,'bck.wav'))    
+background_sound = pygame.mixer.music.load(os.path.join(song_folder,'bck.wav'))  
+power_sound = pygame.mixer.Sound(os.path.join(song_folder,"sfx_zap.ogg"))  
 pygame.mixer.music.set_volume(0.3)
 player_die_sound = pygame.mixer.Sound(os.path.join(song_folder,'shipdes.ogg'))
 explosion_anim = {} 
@@ -74,7 +78,13 @@ class battleship(pygame.sprite.Sprite):
         self.lives = 2
         self.hidden = False
         self.hide_timer = pygame.time.get_ticks()
+        self.power_level = 1
+        self.power_timer  = pygame.time.get_ticks()
+
     def update(self):    
+        if self.power_level >=2 and pygame.time.get_ticks() - self.power_timer > 3000 :
+            self.power_level-=1
+            self.power_timer = pygame.time.get_ticks()
         if self.hidden and pygame.time.get_ticks() - self.hide_timer > 2000 :
             self.hidden = False
             self.rect.centerx =WIDTH/2
@@ -92,14 +102,26 @@ class battleship(pygame.sprite.Sprite):
             self.rect.left = 0    
         self.rect.x+=self.speedx
     def shoot(self):
-        bullet = bullets(self.rect.centerx,self.rect.top)    
-        all_sprites.add(bullet)
-        Bull.add(bullet)
-        shoot_sound.play()
+        if self.power_level==1 :
+            bullet = bullets(self.rect.centerx,self.rect.top)    
+            all_sprites.add(bullet)
+            Bull.add(bullet)
+            shoot_sound.play()
+        if self.power_level>=2 :
+            bullet1 = bullets(self.rect.centerx-25,self.rect.top-20)    
+            bullet2 = bullets(self.rect.centerx+25,self.rect.top-20)
+            all_sprites.add(bullet1)
+            all_sprites.add(bullet2)
+            Bull.add(bullet1)
+            Bull.add(bullet2)
+            shoot_sound.play()    
     def hide(self):
         self.hidden = True
         self.hide_timer = pygame.time.get_ticks()
         self.rect.center = (WIDTH/2,HEIGHT+200)
+    def powerup(self):
+        self.power_level+=1
+        self.power_timer = pygame.time.get_ticks()    
 
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
@@ -136,8 +158,6 @@ class Mob(pygame.sprite.Sprite):
             self.rect.x = random.randrange(0,WIDTH-self.rect.width)
             self.rect.y = random.randrange(-140,-40)
             self.speedy = random.randrange(1,7)
-mobs = pygame.sprite.Group()
-Bull = pygame.sprite.Group()
 class bullets(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
@@ -174,14 +194,21 @@ class explo(pygame.sprite.Sprite):
                 self.image = explosion_anim[self.size][self.frame]
                 self.rect = self.image.get_rect()
                 self.rect.center = center
-
-
-ship = battleship()
-for i in range(7):
-    m = Mob()
-    mobs.add(m)
-    all_sprites.add(m)
-points = 10
+class Power(pygame.sprite.Sprite):
+    def __init__(self,center):
+        pygame.sprite.Sprite.__init__(self)
+        self.type = random.choice(['gun','health','live'])
+        self.image = power_img[self.type]
+        #pygame.Surface((5,8))
+        #self.image.fill((0,100,0))
+        self.image.set_colorkey(black)
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.speedy = 5
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top>HEIGHT:
+            self.kill()            
 font_name = pygame.font.match_font('comic')
 def draw_text(surfacee,text,size,x,y):
     font = pygame.font.Font(font_name,size)
@@ -205,12 +232,41 @@ def draw_lives(surface,x,y,lives,img):
         img_rect.x = x + 20*i
         img_rect.y = y
         surface.blit(img,img_rect)
+def first_screen():
+    screen.blit(background,background_rect)
+    draw_text(screen,'Hue Hue Hue',25,WIDTH/2,HEIGHT/5)
+    draw_text(screen,'SpaceShit!',50,WIDTH/2,2*HEIGHT/5)
+    draw_text(screen,'Arrow Key - Left/Right  : Space - Shoot',25,WIDTH/2,4*HEIGHT/5)
+    draw_text(screen,'Press any key to continue...',20,WIDTH/2,HEIGHT-20)
+    pygame.display.flip()
+    wait = True
+    while wait:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+            if event.type==pygame.KEYUP:
+                wait=False    
 
 #Game loop
-all_sprites.add(ship)
 pygame.mixer.music.play(loops=-1)
 running = True
+game_over = True
 while running:
+    if game_over:
+        first_screen()
+        game_over = False
+        all_sprites= pygame.sprite.Group()
+        mobs = pygame.sprite.Group()
+        Bull = pygame.sprite.Group()
+        PWups = pygame.sprite.Group()
+        ship = battleship()
+        all_sprites.add(ship)
+        for i in range(7):
+            m = Mob()
+            mobs.add(m)
+            all_sprites.add(m)
+        points = 10
     clock.tick(FPS)
     #Events
 
@@ -229,18 +285,23 @@ while running:
     hits = pygame.sprite.groupcollide(mobs,Bull,True,True)
     for hit in hits:
         points+=int(hit.speedy)
+        if random.random() > 0.9 :      
+           Pow = Power(hit.rect.center)
+           all_sprites.add(Pow)
+           PWups.add(Pow) 
         explo_sound.play()
         expl = explo(hit.rect.center,'lar')
         all_sprites.add(expl)
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
+
     hits = pygame.sprite.spritecollide(ship,mobs,True,pygame.sprite.collide_circle)
     for hit in hits : 
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
-        ship.health -= int(abs(hit.speedx) + int(hit.speedy))*2
+        ship.health -= int(abs(hit.speedx) + int(hit.speedy))*4
         if not ship.health < 1: 
             explo_sound.play()
             expl = explo(hit.rect.center,'sml')
@@ -252,15 +313,25 @@ while running:
             ship.hide()
             ship.lives-=1
             ship.health=100
-
+    hits = pygame.sprite.spritecollide(ship,PWups,True)
+    for hit in hits : # gun,live
+        power_sound.play()
+        if hit.type=='health':
+           ship.health += random.randrange(10,30)
+           if ship.health > 100 :
+               ship.health =100
+        elif hit.type == 'live':
+           ship.lives+=1
+        elif hit.type == 'gun' :
+            ship.powerup()
     if ship.lives==0 and not death_explo.alive():
-        running = False      
+        game_over=True     
     screen.fill(white)
     screen.blit(background,background_rect)
     all_sprites.draw(screen)
     draw_text(screen,str(points),20,WIDTH/2,20)
     draw_health(screen,0,HEIGHT-7,ship.health)
-    draw_lives(screen,WIDTH-50,5,ship.lives,ship_tag)
+    draw_lives(screen,WIDTH-75,5,ship.lives,ship_tag)
     #Draw where?
     pygame.display.flip()
 pygame.quit()
